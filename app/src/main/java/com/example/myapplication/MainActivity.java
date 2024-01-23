cf8a0d7ccaa3bd14eca8356bd501f3685a954200
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-01-23 10:30:27 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickHandler(View view) {
+        TextView textBox = findViewById(R.id.textBox);
+        textBox.setText("HI!");
+    }
+}
